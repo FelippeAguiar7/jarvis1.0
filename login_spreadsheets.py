1b984ad18b4e165d1b92d6c0664d136372e059c0
Felippe Aguiar <105329993+FelippeAguiar7@users.noreply.github.com>
2023-09-06 11:53:35 -0300

    Create login_spreadsheets.py

---
+++ login_spreadsheets.py
@@ -0,0 +1,5 @@
+#https://docs.google.com/spreadsheets/d/xxxxxxxxxxxx/edit#gid=0
+#loginSpreadsheet_id xxxxxxxxxxxx Da sua planilha
+#loginSampleRangeName Range de celulas que vc vai lêr no read do google sheet
+loginSpreadsheet_id = 'xxxxxxxxxxxx'
+loginSampleRangeName = 'Página1!A1:C1000'
